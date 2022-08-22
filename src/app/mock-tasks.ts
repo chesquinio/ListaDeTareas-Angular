798b01239dc21165e96e81a4d13eb5ec1d7d146c
francis <willenerfrancis0@gmail.com>
2022-08-22 17:36:35 -0300

    nuevo cambio

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,14 @@
+import {Task} from "./Task"
+
+export const TASKS = [
+    {
+        id: 1,
+        text: "hola pvto",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "hola xd",
+        reminder: false
+    }
+]
