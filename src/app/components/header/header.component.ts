a96a8fba4587a6651a32cc0958efd7f2b684b14e
francis <willenerfrancis0@gmail.com>
2022-08-28 23:50:21 -0300

    Base de datos y Consultas para Agregar tareas TERMINADO

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService} from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'Task List';
+  showAddTask: boolean = false;
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor(
+    private uiService:UiService,
+    private router:Router) { 
+      this.subscription = this.uiService.onToggle().subscribe(value=> this.showAddTask = value);
+    }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(): void {
-    console.log("xd")
+    this.uiService.toggleAddTask();
+  }
+
+  hasRouter(router:string) {
+    return this.router.url === router;
   }
 
 }
